6d705a66a185a9e12d91878449c79579fa676932
taehee <kth7121@gmail.com>
2020-03-08 18:53:50 +0900

    readCSV() 추가

--- src/com/git/util/ReadUtil.java
+++ src/com/git/util/ReadUtil.java
@@ -5,6 +5,7 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 /**
@@ -14,6 +15,51 @@ import java.util.List;
  * @since 2020.01.01
  */
 public class ReadUtil {
+
+	/**
+	 * CSV 파일을 라인별로 맵에 담아 리스트에 세팅한다
+	 * 
+	 * @param filePath
+	 * @return
+	 */
+	public List<HashMap<String, String>> readCSV(String filePath) {
+		List<HashMap<String, String>> result = new ArrayList<HashMap<String, String>> ();
+		
+		// 파일을 헤더까지 포함해서 읽어온다
+		List<String> lineList = readFile(filePath, 0);
+		
+		String[] cols = null;
+		
+		for(int i=0; i<lineList.size(); i++) {
+			String line = lineList.get(i);
+			
+			// 첫번째 줄은 헤더(=컬럼)
+			if(i == 0 && line.trim().length() > 0) {
+				cols = line.trim().split(",");
+			}
+			
+			if(cols != null) {
+				String[] values = null;
+				if(line.endsWith(",")) {
+					values = (line + " ").split(",");
+				} else {
+					values = line.split(",");
+				}
+				
+				if(cols.length == values.length) {
+					HashMap<String, String> map = new HashMap<String, String> ();
+					for(int j=0; j<values.length; j++) {
+						map.put(cols[j], values[j]);
+					}
+					
+					result.add(map);
+				}
+			}
+		}
+		
+		return result;
+	}
+	
 	
 	/**
 	 * 파일 내용을 List에 담는다
